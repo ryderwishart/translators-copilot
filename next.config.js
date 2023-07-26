bc5bdca088afd824d78f7fba3c31e331219786c7
ryderwishart <ryderwishart@gmail.com>
2023-07-26 08:17:27 -0700

    Update port to 8080

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
         source: "/api/:path*",
         destination:
           process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/api/:path*"
+            ? "http://127.0.0.1:8080/api/:path*"
             : "/api/",
       },
     ];
