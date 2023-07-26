4514b04586006e8a7b17b4f2ce99b1763d0a3ee6
ryderwishart <ryderwishart@gmail.com>
2023-07-26 08:25:09 -0700

    Add function to get entire bible and save triples to disk

--- api/index.py
+++ api/index.py
@@ -88,3 +88,50 @@ def get_verse(full_verse_ref: str, language_code: str):
         }
     }
 
+# get the entire bible with source text and gloss, bsb english, and target language
+@app.get("/api/bible/{language_code}")
+def get_bible(language_code: str):
+    """
+    Get the entire Bible from bsb_bible_df, 
+    AND macula_df (greek and hebrew)
+    AND target_vref_data (target language)
+    
+    e.g., http://localhost:3000/api/bible/aai
+    """
+    import json 
+    
+    target_vref_df = get_target_vref_df(language_code)
+    
+    output = []
+    for i in range(len(bsb_bible_df)):
+        bsb_row = bsb_bible_df.iloc[i]
+        macula_row = macula_df.iloc[i]
+        target_row = target_vref_df.iloc[i]
+        
+        output.append({
+            'bsb': {
+                'verse_number': int(bsb_row.name),
+                'vref': bsb_row['vref'],
+                'content': bsb_row['content']
+            },
+            'macula': {
+                'vref': macula_row['vref'],
+                'verse_number': int(macula_row.name),
+                'content': macula_row['content']
+            },
+            'target': {
+                'verse_number': int(target_row.name),
+                'vref': target_row['vref'],
+                'content': target_row['content']
+            }
+        })
+    
+    # Save output to disk as `data/triples/{language_code}.json`
+    if not os.path.exists('data/triples'):
+        os.mkdir('data/triples')
+        
+    with open(f'data/triples/{language_code}.json', 'w') as f:
+        json.dump(output, f, ensure_ascii=False, indent=4)
+    
+    return True
+
