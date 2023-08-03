3dc8ad3c894a0df77d672fd3fee4750a1d9e5859
ryderwishart <ryderwishart@gmail.com>
2023-08-03 11:22:06 -0700

    Make sure next config works with both APIs

--- next.config.js
+++ next.config.js
@@ -3,11 +3,15 @@ const nextConfig = {
   rewrites: async () => {
     return [
       {
+        source: "/api/chat",
+        destination: "/api/chat"
+      },
+      {
         source: "/api/:path*",
         destination:
           process.env.NODE_ENV === "development"
             ? "http://127.0.0.1:8080/api/:path*"
-            : "/api/",
+            : "/api/:path*",
       },
     ];
   },
