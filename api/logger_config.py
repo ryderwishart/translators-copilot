aee234a5eff03730c5b46587546596811a43ddf4
ryderwishart <ryderwishart@gmail.com>
2023-08-03 14:13:36 -0700

    Update logger again
    Still not overriding…

--- api/logger_config.py
+++ api/logger_config.py
@@ -1,30 +1,47 @@
-from loguru import logger
-import sys
+import logging
 
 class CustomColors:
-    CRITICAL = "red"
-    ERROR = "red"
-    WARNING = "yellow"
-    SUCCESS = "green"
-    INFO = "blue"
-    DEBUG = "magenta"
+    CRITICAL = "\033[1;31m"  # Bright Red
+    ERROR = "\033[0;31m"    # Red
+    WARNING = "\033[1;33m"  # Bright Yellow
+    INFO = "\033[0;34m"     # Blue
+    DEBUG = "\033[0;35m"    # Magenta
+    RESET = "\033[0m"       # Reset
 
-log_format = (
-    "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | "
-    "<level>{level: <8}</level> | "
-    "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - "
-    "<level>{message}</level>"
-)
+LOG_COLORS = {
+    'CRITICAL': CustomColors.CRITICAL,
+    'ERROR': CustomColors.ERROR,
+    'WARNING': CustomColors.WARNING,
+    'INFO': CustomColors.INFO,
+    'DEBUG': CustomColors.DEBUG,
+}
 
-logger.remove()
-logger.add(sys.stderr, format=log_format, level="DEBUG", colorize=True, diagnose=False,
-    enqueue=True, backtrace=True,
-    filter=lambda record: record["level"].name == "DEBUG")
+class ColoredFormatter(logging.Formatter):
+    def format(self, record):
+        log_msg = logging.Formatter.format(self, record)
+        return LOG_COLORS.get(record.levelname, CustomColors.RESET) + log_msg + CustomColors.RESET
 
-logger.level("CRITICAL", color="<red>")
-logger.level("ERROR", color="<red>")
-logger.level("WARNING", color="<yellow>")
-logger.level("SUCCESS", color="<green>")
-logger.level("INFO", color="<blue>")
-logger.level("DEBUG", color="<magenta>")
+# Create a console handler with a custom format
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(ColoredFormatter())
 
+# Get the "uvicorn.access" logger and configure it
+access_logger = logging.getLogger('uvicorn.access')
+access_logger.handlers = [console_handler]
+
+# Get the "uvicorn" logger and configure it
+uvicorn_logger = logging.getLogger('uvicorn')
+uvicorn_logger.handlers = [console_handler]
+
+def setup_logger(name, level=logging.DEBUG):
+    handler = logging.StreamHandler()
+    handler.setFormatter(ColoredFormatter())
+
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    logger.handlers = [handler]
+
+    return logger
+
+access_logger = setup_logger('uvicorn.access')
+uvicorn_logger = setup_logger('uvicorn')
