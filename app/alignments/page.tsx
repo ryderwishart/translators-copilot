ccd61cf6fc34bac47986946fb574df480f265b5b
Chase Helton <jchasehelton@gmail.com>
2023-09-15 16:21:29 -0400

    Update alignments

--- app/alignments/page.tsx
+++ app/alignments/page.tsx
@@ -1,65 +1,59 @@
 'use client';
-// interface DataProps {
-//   vref: string;
-//   target_language_code: string;
-// }
+import AlignmentTable, { AlignmentSet } from '@/components/AlignmentTable';
+import { Button } from '@/components/ui/button';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { zodResolver } from '@hookform/resolvers/zod';
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
 
-export interface AlignmentSet {
-  vref: string;
-  alignments: [Alignment];
-}
+const formSchema = z.object({
+  url: z.string({
+    required_error: "Please enter URL."
+  }),
+})
 
-export interface Alignment {
-  source: string;
-  target: string;
-  bridge: string;
-}
+export default function Page() {
+  const [alignmentData, setAlignmentData] = React.useState<AlignmentSet[]>([]);
 
-interface AlignmentTableProps {
-  alignmentData: AlignmentSet[];
-}
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    reValidateMode: "onSubmit",
+  })
 
-const AlignmentTable: React.FC<AlignmentTableProps> = ({ alignmentData }) => {
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    //  { values } will have the 'url' field
+    const data = await getData('spapddpt_split', 10);
+    setAlignmentData(data);
+  };
+
   return (
-    <div className="flex flex-col items-center self-center bg-white shadow-md rounded-lg p-4 items-stretch">
-      {/* {JSON.stringify(alignmentResponse)} */}
-      {/** create a table to view alignments */}
-      {alignmentData ? (
-        alignmentData.map((alignmentSet: AlignmentSet) => (
-          <div key={alignmentSet.vref} className="my-4">
-            <div className="font-semibold text-lg mb-2">
-              {alignmentSet.vref}
-            </div>
-            <table className="table-auto w-full">
-              <thead>
-                <tr className="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
-                  <th className="py-3 px-6 text-left w-1/3">Source</th>
-                  <th className="py-3 px-6 text-left w-1/3">Bridge</th>
-                  <th className="py-3 px-6 text-left w-1/3">Target</th>
-                </tr>
-              </thead>
-              <tbody>
-                {alignmentSet.alignments.map((alignment: Alignment) => (
-                  <tr
-                    key={alignment.source}
-                    className="text-gray-600 text-sm font-light border-b border-gray-200"
-                  >
-                    <td className="py-3 px-6 w-1/3">{alignment.source}</td>
-                    <td className="py-3 px-6 w-1/3">{alignment.bridge}</td>
-                    <td className="py-3 px-6 w-1/3">{alignment.target}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-        ))
-      ) : (
-        <div className="text-center py-4">Loading...</div>
-      )}
+    <div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="border-2 rounded-md p-4 space-y-6">
+          <FormField
+            control={form.control}
+            name="url"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>URL</FormLabel>
+                <FormControl>
+                  <div className='flex flex-row'>
+                    <Input className='mr-2' placeholder="Enter URL here..." {...field} />
+                    <Button type="submit">Submit</Button>
+                  </div>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </form>
+      </Form>
+      <AlignmentTable alignmentData={alignmentData} />
     </div>
   );
-};
+}
 
 async function getData(
   language_code: string,
@@ -81,34 +75,4 @@ async function getData(
 
   console.log({ alignmentArray });
   return alignmentArray;
-}
-
-export default function Page() {
-  const [alignmentData, setAlignmentData] = React.useState<AlignmentSet[]>([]);
-  const [url, setUrl] = React.useState(
-    'http://localhost:3000/api/get_alignment',
-  );
-
-  const handleSubmit = async (event: React.FormEvent) => {
-    event.preventDefault();
-    const data = await getData('spapddpt_split', 10);
-    setAlignmentData(data);
-  };
-
-  return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <label>
-          URL:
-          <input
-            type="text"
-            value={url}
-            onChange={(e) => setUrl(e.target.value)}
-          />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-      <AlignmentTable alignmentData={alignmentData} />
-    </div>
-  );
 }
