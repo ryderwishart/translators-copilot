cf5028f22200ce032a1c5e768793832ee4ca679f
Ben Scholtens <ben.scholtens@me.com>
2023-10-16 11:45:50 -0400

    get tests working with jsonl

--- scripts/find-ranges-for-alignments/tests/test_script.py
+++ scripts/find-ranges-for-alignments/tests/test_script.py
@@ -13,21 +13,28 @@ class TestFindRangesForAlignments(unittest.TestCase):
         # print(result.stdout)
         
         with open('scripts/find-ranges-for-alignments/tests/sample_ingestion_file_final_output.jsonl', 'r') as f:
-            output = f.read()
-
+            actual_output = [json.loads(line) for line in f]
         # Check if the script executed successfully
         # self.assertEqual(result.returncode, 0, f"Script failed with error: {result.stderr}")
 
         # Load the expected output from a file (you'll need to create this file)
+        expected_output = []
         with open('scripts/find-ranges-for-alignments/tests/expected_output.jsonl', 'r') as f:
-            expected_output = f.read()
+            for i, line in enumerate(f, start=1):
+                try:
+                    expected_output.append(json.loads(line))
+                except json.JSONDecodeError:
+                    print(f"Error decoding JSON on line {i}: {line}")
+                    raise
+        # with open('scripts/find-ranges-for-alignments/tests/expected_output.jsonl', 'r') as f:
+        #     expected_output = [json.loads(line) for line in f]
 
         # Convert both the actual and expected outputs to dictionaries
-        actual_output_dict = json.loads(output)
-        expected_output_dict = json.loads(expected_output)
+        # actual_output_dict = json.loads(actual_output)
+        # expected_output_dict = json.loads(expected_output)
 
         # Check that the script's output matches the expected output
-        self.assertEqual(expected_output_dict, actual_output_dict)
+        self.assertEqual(actual_output, expected_output)
 
 if __name__ == '__main__':
     unittest.main()
